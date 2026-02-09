6f313709e48b4ac3e976025eb7df9424e4843df9
July-h5kf3 <2312810@mail.nankai.edu.cn>
2026-02-09 14:47:55 +0800

    modified:   assignment1-basics/cs336_basics/Cross_entropy.py

--- assignment1-basics/cs336_basics/Cross_entropy.py
+++ assignment1-basics/cs336_basics/Cross_entropy.py
@@ -7,8 +7,9 @@ def cross_entropy(logits, targets):
     targets: Int[Tensor, "batch"]
     """
     max_logits = logits.max(dim = -1,keepdim = True).values #shape: [batch_size,1]
-    logits = logits - max_logits
-    
-    log_sum_exp = torch.log(torch.exp(logits).sum(dim=-1))  # [batch]
+    #如果直接对logits减去最大值，最后会因为log操作出现下溢的情况
+    log_sum_exp = torch.log(torch.exp(logits - max_logits).sum(dim=-1,keepdim=True)) + max_logits  # [batch,1]
+    # [batch,1] -> [batch]
+    log_sum_exp = rearrange(log_sum_exp,'b 1 -> b')
     loss = log_sum_exp - logits[torch.arange(logits.shape[0]),targets]
     return loss.mean()
