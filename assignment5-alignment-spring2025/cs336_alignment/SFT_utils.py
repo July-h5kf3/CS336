1f709e621bd957ba2c8c8e8eab0621061a459838
July-h5kf3 <2312810@mail.nankai.edu.cn>
2026-06-27 15:04:39 +0800

    tokenizer_prompt_and_output

---
+++ assignment5-alignment-spring2025/cs336_alignment/SFT_utils.py
@@ -0,0 +1,46 @@
+import torch
+from transformers import AutoTokenizer
+
+def tokenizer_prompt_and_output(
+    prompt_strs: list[str],
+    output_strs: list[str],
+    tokenizer,
+):
+    prompt_token_ids = tokenizer(prompt_strs, add_special_tokens = False)["input_ids"]
+    output_token_ids = tokenizer(output_strs, add_special_tokens = False)["input_ids"]
+
+    pad_token_id = tokenizer.pad_token_id
+
+    full_seqs = []
+    full_seq_masks = []
+    for prompt_ids, output_ids in zip(prompt_token_ids, output_token_ids):
+        sequence = prompt_ids + output_ids
+        mask = (
+            [False] * len(prompt_ids) + [True] * len(output_ids)
+        )
+        full_seqs.append(sequence)
+        full_seq_masks.append(mask)
+    max_length = max(len(sequence) for sequence in full_seqs)
+    padded_seqs = []
+    padded_mask = []
+    for seq,mask in zip(full_seqs, full_seq_masks):
+        padding_length = max_length - len(seq)
+        padded_seqs.append(
+            seq + [pad_token_id] * padding_length
+        )
+        padded_mask.append(
+            mask + [False] * padding_length
+        )
+    token_ids = torch.tensor(padded_seqs, dtype = torch.long)
+    full_seq_masks = torch.tensor(padded_mask, dtype = torch.bool)
+
+    input_ids = token_ids[:, :-1]
+    labels = token_ids[:,1:]
+
+    response_mask = full_seq_masks[:,1:]
+
+    return {
+        "input_ids": input_ids,
+        "labels": labels,
+        "response_mask": response_mask
+    }
